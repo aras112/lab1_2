9f5f6e505ccb7ef713b468d4638c8ee360d402ab
aras112nowe@o2.pl <9018a9de>
2019-03-20 18:58:06 +0100

    default method in interface TaxCalculator deleted

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -5,32 +5,5 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import java.math.BigDecimal;
 
 interface TaxCalculator {
-
-    default TaxInfoForCountry calculate(ProductType productType) {
-        TaxInfoForCountry taxInfoForCountry=new TaxInfoForCountry();
-
-        switch (productType) {
-            case FOOD: {
-                taxInfoForCountry.setRatio(new BigDecimal("0.07"));
-                taxInfoForCountry.setDescription("7% (F)");
-                break;
-            }
-
-            case STANDARD:
-                taxInfoForCountry.setRatio(new BigDecimal("0.23"));
-                taxInfoForCountry.setDescription("23%");
-                break;
-
-            case DRUG: {
-                taxInfoForCountry.setRatio(new BigDecimal("0.05"));
-                taxInfoForCountry.setDescription("5% (D)");
-                break;
-            }
-
-            default: {
-                throw new IllegalArgumentException(productType + " not handled");
-            }
-        }
-        return taxInfoForCountry;
-    }
+    TaxInfoForCountry calculate(ProductType productType);
 }
