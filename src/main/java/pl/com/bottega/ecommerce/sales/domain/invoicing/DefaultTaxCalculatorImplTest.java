58344b09813b26982c2985f5b9e42edda61b07c1
aras112nowe@o2.pl <9018a9de>
2019-03-27 18:21:35 +0100

    DefaultTaxCalculatorImpl

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorImplTest.java
@@ -0,0 +1,30 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DefaultTaxCalculatorImplTest {
+
+    @Test public void simpleUseCalculate() {
+        DefaultTaxCalculatorImpl defaultTaxCalculator = new DefaultTaxCalculatorImpl();
+        TaxInfoForCountry taxInfo= defaultTaxCalculator.calculate(ProductType.DRUG);
+        Assert.assertThat(taxInfo.getRatio(),is(new BigDecimal("0.05")));
+    }
+
+
+    @Test(expected = IllegalArgumentException.class) public void calculateIncorrectProductType() {
+        DefaultTaxCalculatorImpl defaultTaxCalculator = new DefaultTaxCalculatorImpl();
+        TaxInfoForCountry taxInfo = defaultTaxCalculator.calculate(ProductType.valueOf("123"));
+    }
+
+    @Test(expected = NullPointerException.class) public void calculateNullProductType() {
+        DefaultTaxCalculatorImpl defaultTaxCalculator = new DefaultTaxCalculatorImpl();
+        TaxInfoForCountry taxInfo = defaultTaxCalculator.calculate(null);
+    }
+}
